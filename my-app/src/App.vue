28b1b89ab28fba64aaab1304dd02edf3a94599c2
nvonbuttlar <nick.von.buttlar@gmail.com>
2023-06-13 17:01:42 -0700

    base level styles

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -2,6 +2,10 @@
 <!-- https://medium.com/bb-tutorials-and-thoughts/how-to-develop-and-build-vue-js-app-with-nodejs-backend-typescript-version-2eeb0f10e87f -->
 <!-- https://vueschool.io/articles/vuejs-tutorials/how-to-use-vue-router-a-complete-tutorial/ -->
 
+<script setup>
+  // import "@fontsource/open-sans/500.css";
+</script>
+
 <template>
   <div>
     <nav>
@@ -15,13 +19,28 @@
 </template>
 
 <style>
-#app {
+body {
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #FFF;
 }
+
+#app {
+  height: 100vh;
+  text-align: center;
+  background-color: #2c3e50;
+}
+
+/* h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: 'Open Sans';
+} */
 
 nav {
   padding: 30px;
@@ -29,7 +48,12 @@ nav {
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #FFF;
+}
+
+a:visited {
+  font-weight: bold;
+  color: #FFF;
 }
 
 nav a.router-link-exact-active {
